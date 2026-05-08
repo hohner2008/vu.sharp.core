19f2cc6b59c746f175327a93e766f4c1b1828df8
lisa <hohner2008@gmail.com>
2026-05-08 20:23:05 +0300

    add url property to BaseCrawler

--- vu.core/BaseCrawler.cs
+++ vu.core/BaseCrawler.cs
@@ -2,5 +2,15 @@ namespace vu.core;
 
 public class BaseCrawler
 {
+    private Uri _baseUri;
     
+    public string Url { 
+        get => _baseUri.ToString(); 
+        set  => _baseUri = new Uri(value); 
+    }
+
+    public BaseCrawler(Uri baseUri)
+    {
+        _baseUri = baseUri;
+    }
 }
